146102c79e5e1e0f7ec1436fb25a7aebb5891c82
JungMongKyo <dreamong91@gmail.com>
2018-09-27 17:20:51 +0900

    'timezone'

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import timezone
 
-# Create your views here.
+
+def post_list(request):
+    # 현재 지역에 맞는 날짜&시간 객체 할당
+    current_time = timezone.now()
+
+    return HttpResponse(
+        '<html>'
+        '<body>'
+        '<h1>Post List</h1>'
+        '<p>{}</p>'
+        '</body>'
+        '</html>'.format(
+            # 날짜&시간 객체가 가진 정보를 문자열로 반환
+            current_time.strftime('%Y. %m. %d<br>%H:%M:%S')
+        )
+    )
