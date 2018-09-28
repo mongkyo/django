6d54c284944cd995538c402cfda58052f7e580c4
JungMongKyo <dreamong91@gmail.com>
2018-09-28 16:54:45 +0900

    post_detail 파일 연동

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.models import User
+import re
+
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.utils import timezone
 
 from .models import Post
 
@@ -11,3 +12,21 @@ def post_list(request):
         'posts': posts,
     }
     return render(request, 'blog/post_list.html', context)
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(id=pk)
+    context = {
+        'post': post,
+    }
+    # templates/blog/post_detail.html
+    #  post가 가진 title, text, author, created_date, published_date를 적절히 출력
+    # request.path에 있는 문자열을
+    #   /posts/12345/
+    #   /posts/3/
+    # 위의 경우 정규표현식으로 적절히
+    # 12345와 3문자열을 추출
+    # 추출한 결과를 HttpResponse에 리턴
+    # m = re.search(r'^/posts/(?P<pk>\d+)/$', request.path)
+    # pk = m.group('pk')
+    return render(request, 'blog/post_detail.html', context)
--- app/config/urls.py
+++ app/config/urls.py
@@ -21,4 +21,17 @@ from blog import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.post_list),
+    url(r'^posts/(?P<pk>\d+)/$', views.post_detail)
+
+    # request가 오면
+    # post_detail(request=request, pk=<그룹부분에 주어진 값>)
+
+    # () 그룹화를 한 부분은 뒷 부분에 전달할 때 추가 인수로 전달된다
+
+    # /posts/1/
+    # /posts/3/
+    # /posts/9999/
+    # <- 이 경우에 해당하는 정규표현식 패턴이
+    # blog.views.post_detail <- 여기로 전달되도록 설정 및 뷰 함수 구현
+    # -> 이 뷰는 'Post Detail'이라는 문자열을 httpResponse를 사용해 리턴
 ]
