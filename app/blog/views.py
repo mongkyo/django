493789e0534197eec819213449bfb724f21f6b15
JungMongKyo <dreamong91@gmail.com>
2018-09-28 14:23:31 +0900

    for loop를 post_list.html 템플릿에서 실행

--- app/blog/views.py
+++ app/blog/views.py
@@ -7,13 +7,7 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.order_by('-created_date')
-    content = ''
-    content += '<ul>'
-    for post in posts:
-        content += f'<li>{post.title}</li>'
-    content += '</ul>'
-
     context = {
-        'posts': content,
+        'posts': posts,
     }
     return render(request, 'blog/post_list.html', context)
