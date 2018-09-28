f7c0802bf0e05201113d398e89431158f52ab0aa
JungMongKyo <dreamong91@gmail.com>
2018-09-28 14:20:23 +0900

    post_list view에서 리스트 html생성

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,36 +1,19 @@
-import random
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.utils import timezone
 
-from django.http import HttpResponse
-from django.template import loader
+from .models import Post
 
 
 def post_list(request):
-    # # 템플릿을 가져옴 (단순 문자열이 아님)
-    # template = loader.get_template('blog/post_list.html')
-    # # 해당 템플릿을 렌더링
-    # context = {}
-    # content = template.render(context, request)
-    # return HttpResponse(content)
+    posts = Post.objects.order_by('-created_date')
+    content = ''
+    content += '<ul>'
+    for post in posts:
+        content += f'<li>{post.title}</li>'
+    content += '</ul>'
 
     context = {
-        'pokemon' : random.choice(['피카츄', '파이리', '꼬부기'])
+        'posts': content,
     }
-
-    # render 함수
-    # 1번째 인수로 자신의 view의 첫 번째 매개변수인 request를 전달
-    # 2번째 인수로 템플릿파일의 경로를 전달
-    # 3번째 인수(선택)로 dict를 전달
-    # -> 템플릿파일의 경로에 있는 HTML을 가져와서 {{변수}}와 같은 부분들에 동적으로 문자열을 생성
-    #     생성된 결과를 HttpResponse로 돌려줌, 브라우저는 해당 결과를 받아 사용자에게 보여주게 됨
-    # loader.get_template
-    # template.render
-    # HttpResponse
-
-    return render(
-        request = request,
-        template_name = 'blog/post_list.html',
-        context = context,
-    )
-    # 줄여서 쓰면
-    # return render(request, 'blog/post_list.html', context)
+    return render(request, 'blog/post_list.html', context)
